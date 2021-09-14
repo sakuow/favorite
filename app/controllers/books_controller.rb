73b9c837d83bf43e712c720b7210505e3f71cec5
sakuow <yusaku1335@gmail.com>
2021-09-14 00:47:49 +0000

    second commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,16 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/index'
+  get 'books/create'
+  get 'books/show'
+  get 'books/destroy'
+  get 'homes/top'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
