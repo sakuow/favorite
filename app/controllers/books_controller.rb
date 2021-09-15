55530ba8fcc3cb02479b35488370e65711279801
sakuow <yusaku1335@gmail.com>
2021-09-15 13:16:46 +0000

    sixth commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,10 +24,13 @@ class BooksController < ApplicationController
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
  private
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body,)
   end
 end
