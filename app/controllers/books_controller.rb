709fbe52a99daf2d211d420c075f2399b7dc7fd4
sakuow <yusaku1335@gmail.com>
2021-09-15 15:43:29 +0000

    seventh commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,8 +29,18 @@ class BooksController < ApplicationController
     redirect_to books_path
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
  private
   def book_params
-    params.require(:book).permit(:title, :body,)
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
+    @books = Book.all
   end
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.save
+    redirect_to user_path(@user.id)
   end
 
   def index
@@ -14,7 +22,9 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
+
 end
