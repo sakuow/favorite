d216e14bf77975879d5b678342067eb2ea502cef
sakuow <yusaku1335@gmail.com>
2021-09-16 07:49:18 +0000

    eigth commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,22 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = Book.all
+    @books = @user.books
+    @book = Book.new
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
   end
+
 
   def update
     @user = User.find(params[:id])
-    @user.save
+    if @user.update(user_params)
     redirect_to user_path(@user.id)
+    else
+    render :edit
+    end
   end
 
   def index
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '09b5b45497d65ebe6d4777cbd0c9a733cec8d45636036b79bfd14a711ce265be0f8ccc886341c62d34cb0c814426d2210a0a79e1d09fac076c234b320ec877f4'
